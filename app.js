1b0d1477d8d151a5303be9d2b340af3ed25cd9f4
NicoMantillaM <160680324+NicoMantillaM@users.noreply.github.com>
2024-09-02 01:41:23 -0500

    release: :bookmark: Asignación de Asientos
    feat: :sparkles: caso 3
    
    Se agregó el módulo reserva, y la funcionalidad de selección y reserva de asientos para una proyección específica, se actualizó app.js para usarlo
    
    Hash: 115b740862714d150b0f8d82bc4a9c79294b101f
    
    feat: :sparkles: caso 3.2
    
    Se implementó el método `cancelarReserva` en la clase `reserva`, permitiendo la cancelación de reservas de asientos previamente realizadas. Además, se actualizó `app.js` para integrar y habilitar la funcionalidad de cancelación de reservas en el flujo de la aplicación.
    
    Hash: 3418869206b652e5465d7f638b8980d8fdf74029
    
    feat: :sparkles: Crear asiento
    
    La clase asiento tiene dos métodos principales: consultarDisponibilidadAsientos y addAsiento. Estos métodos realizan operaciones CRUD en la base de datos, verificando la existencia de salas y horarios de función, y manejan errores.
    
    Hash: bfbf284cbc89b2d600bf38cb807587e4cfbae344
    
    docs: :memo: Documentacion de asiento
    
    README.md actualizado con documentación de la clase asiento, app.js modificado y módulo asiento.js refactorizado con comentarios agregados y manejo de errores
    
    Hash: c139c6534221ca66b3700234ba99c355f52bbe43

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ const funcion = require('./js/module/horario_funcion');
 const pelicula = require('./js/module/pelicula');
 const boleta = require('./js/module/boleta');
 const asiento = require('./js/module/asiento');
+const reserva = require('./js/module/reserva');
 
 
 // let obj = new funcion();
 // obj.getFuncionCartelera().then(res=>{console.log(res)})
 
+// let nuevaFuncion = {
+//     id_sala: "66cfeee58d26b5da40f46c26",
+//     fecha_proyeccion: "18/09/2024",
+//     hora_inicio: "13:20",
+//     hora_fin: "14:50",
+//     id_pelicula: "66cfec618d26b5da40f46c1f"
+// }
+// obj.addFuncion(nuevaFuncion).then(res=>{console.log(res)})
 
 // let obj = new pelicula();
 // let id_pelicula = ("66cfec618d26b5da40f46c21")
@@ -23,12 +32,37 @@ const asiento = require('./js/module/asiento');
 // let id_reserva = null
 // let metodo_pago = ("tarjeta de credito")
 
-// obj.comprarBoleta(id_horario_funcion, asientos, id_usuario, id_reserva, metodo_pago)
+// obj.comprarBoleta(id_horario_funcion, asientos, id_usuario, id_reserva, metodo_pago).then(res=>{console.log(res)})
+
 
 
 
 let obj = new asiento();
-let id_sala = ("66cfeee58d26b5da40f46c2b")
-let id_horario_funcion = ("66cff2dc8d26b5da40f46c3d") 
+// let id_sala = ("66cfeee58d26b5da40f46c2b")
+// let id_horario_funcion = ("66cff2dc8d26b5da40f46c3d") 
 
-obj.consultarDisponibilidadAsientos(id_sala, id_horario_funcion).then(res=>{console.log(res)})
+// obj.consultarDisponibilidadAsientos(id_sala, id_horario_funcion).then(res=>{console.log(res)})
+let nuevoAsiento = {
+    id_sala: "66cfeee58d26b5da40f46c2b",
+    lugar: "A8",
+    tipo_lugar: "general",
+    disponibilidad: "disponible",
+    fila: "A",
+    precio: 12000,
+    id_horario_funcion: "66cff2dc8d26b5da40f46c3d",
+};
+obj.addAsiento(nuevoAsiento).then(res=>{console.log(res)})
+
+
+// let obj = new reserva();
+// let id_usuario = ("66cfe4288d26b5da40f46c1e")
+// let asientos = ['66d1bbcbcbb9384d08cf2b8a', '66d1bbcbcbb9384d08cf2b8d']; 
+// let id_horario_funcion = ("66cff2dc8d26b5da40f46c3d") 
+
+// obj.createReserva( id_usuario, asientos, id_horario_funcion).then(res=>{console.log(res)})  
+
+
+let id_reserva = ("66d560f7b70f5e9cdd4a73a8")
+let id_usuario = ("66cfe4288d26b5da40f46c1e")
+
+obj.cancelarReserva( id_reserva, id_usuario).then(res=>{console.log(res)})  
--- js/module/asiento.js
+++ js/module/asiento.js
@@ -1,59 +1,144 @@
+// Importa la conexión a la base de datos desde el archivo 'connect.js'
+const connect = require('../../db/connect/connect');
 
-const connect = require ('../../db/connect/connect')
-
+// Importa la clase ObjectId desde el módulo 'mongodb' para manejar identificadores únicos
 const { ObjectId } = require('mongodb');
 
-module.exports = class asiento extends connect{
-    colletionAsiento
-    constructor(){
-        super()
+module.exports = class asiento extends connect {
+    colletionAsiento; // Declara una propiedad para la colección de asientos
+
+    /**
+     * Constructor de la clase asiento.
+     * Llama al constructor de la clase padre para establecer la conexión a la base de datos.
+     */
+    constructor() {
+        super(); // Llama al constructor de la clase padre 'connect'
     }
 
+    /**
+     * Consulta la disponibilidad de los asientos en una sala específica para un horario de función.
+     * @async
+     * @param {string} id_sala - El ID de la sala.
+     * @param {string} id_horario_funcion - El ID del horario de la función.
+     * @returns {Promise<Object[]>} Un array con los asientos disponibles.
+     * @throws {Error} Si el ID de la sala o el horario de la función no existen o no coinciden.
+     */
     async consultarDisponibilidadAsientos(id_sala, id_horario_funcion) {
-
-        await this.open();
+        await this.open(); // Abre la conexión a la base de datos
         try {
+            // Obtiene las colecciones 'horario_funcion' y 'sala' de la base de datos
             const collectionHorarioFuncion = this.db.collection('horario_funcion'); 
             const collectionSala = this.db.collection('sala'); 
 
             // Verifica si el id_sala existe en la colección 'sala'
             const salaExiste = await collectionSala.findOne({ _id: new ObjectId(id_sala) });
             if (!salaExiste) {
-                throw new Error('El id_sala proporcionado no existe.');
+                throw new Error('El id_sala proporcionado no existe.'); // Lanza un error si la sala no existe
             }
 
             // Verifica si el id_horario_funcion existe en la colección 'horario_funcion'
             const horarioExiste = await collectionHorarioFuncion.findOne({ _id: new ObjectId(id_horario_funcion) });
             if (!horarioExiste) {
-                throw new Error('El id_horario_funcion proporcionado no existe.');
+                throw new Error('El id_horario_funcion proporcionado no existe.'); // Lanza un error si el horario de función no existe
             }
 
-            // Verifica si el id_sala concuerda con el que está en horario_funcion
+            // Verifica si el id_sala concuerda con el que está en 'horario_funcion'
             const funcion = await collectionHorarioFuncion.findOne({
                 _id: new ObjectId(id_horario_funcion),
                 id_sala: new ObjectId(id_sala)
             });
 
             if (!funcion) {
-                throw new Error('No hay ninguna función asignada a esa sala.');
+                throw new Error('No hay ninguna función asignada a esa sala.'); // Lanza un error si la sala no coincide con el horario
             }
 
+            // Obtiene la colección 'asiento' de la base de datos
             const collectionAsiento = this.db.collection('asiento');
 
+            // Busca los asientos disponibles para la sala y el horario especificados
             const res = await collectionAsiento.find({
-                id_sala: new ObjectId(id_sala),                
-                id_horario_funcion: new ObjectId(id_horario_funcion), 
-                disponibilidad: 'disponible'                   
-            }).toArray();                                      
+                id_sala: new ObjectId(id_sala),
+                id_horario_funcion: new ObjectId(id_horario_funcion),
+                disponibilidad: 'disponible'
+            }).toArray();
 
-            this.connection.close();
-            return res; 
+            this.connection.close(); // Cierra la conexión a la base de datos
+            return res; // Retorna los asientos disponibles
+
+        } catch (error) {
+            console.log(error); // Muestra el error en la consola
+        }
+    }
+
+    /**
+     * Agrega un nuevo asiento a la base de datos.
+     * @async
+     * @param {Object} nuevoAsiento - El objeto con los datos del nuevo asiento.
+     * @param {string} nuevoAsiento.id_sala - El ID de la sala.
+     * @param {string} nuevoAsiento.id_horario_funcion - El ID del horario de la función.
+     * @param {string} nuevoAsiento.lugar - El lugar específico del asiento.
+     * @returns {Promise<Object>} Un mensaje de éxito si se agrega el asiento correctamente.
+     * @throws {Error} Si la sala, el horario de función no existen, no coinciden, o si el asiento ya existe.
+     */
+    async addAsiento(nuevoAsiento) {
+        await this.open(); // Abre la conexión a la base de datos
+        try {
+            // Obtiene las colecciones 'sala' y 'horario_funcion' de la base de datos
+            const collectionSala = this.db.collection('sala');
+            const collectionHorarioFuncion = this.db.collection('horario_funcion');
+            this.collectionAsiento = this.db.collection('asiento'); // Define la colección 'asiento'
+
+            // Convierte id_sala y id_horario_funcion a ObjectId
+            nuevoAsiento.id_sala = new ObjectId(nuevoAsiento.id_sala);
+            nuevoAsiento.id_horario_funcion = new ObjectId(nuevoAsiento.id_horario_funcion);
+
+            // Verifica si la sala existe
+            const sala = await collectionSala.findOne({ _id: nuevoAsiento.id_sala });
+            if (!sala) {
+                throw new Error('La sala especificada no existe.'); // Lanza un error si la sala no existe
+            }
+
+            // Verifica si el horario de función existe
+            const horarioFuncion = await collectionHorarioFuncion.findOne({ _id: nuevoAsiento.id_horario_funcion });
+            if (!horarioFuncion) {
+                throw new Error('El horario de función especificado no existe.'); // Lanza un error si el horario no existe
+            }
+
+            // Verifica si el id_sala coincide con el id_horario_funcion
+            const funcion = await collectionHorarioFuncion.findOne({
+                _id: nuevoAsiento.id_horario_funcion,
+                id_sala: nuevoAsiento.id_sala
+            });
+
+            if (!funcion) {
+                throw new Error('El id_sala no coincide con el id_horario_funcion.'); // Lanza un error si la sala y el horario no coinciden
+            }
+
+            // Verifica si el asiento ya existe para esa función
+            const asientoExistente = await this.collectionAsiento.findOne({
+                id_sala: nuevoAsiento.id_sala,
+                id_horario_funcion: nuevoAsiento.id_horario_funcion,
+                lugar: nuevoAsiento.lugar
+            });
+
+            if (asientoExistente) {
+                throw new Error('El asiento ya existe para esa función.'); // Lanza un error si el asiento ya está registrado
+            }
+
+            // Inserta el nuevo asiento en la colección
+            const res = await this.collectionAsiento.insertOne(nuevoAsiento);
+            if (!res.insertedId) {
+                throw new Error('No se pudo agregar el asiento.'); // Lanza un error si la inserción falla
+            }
+
+            await this.connection.close(); // Cierra la conexión a la base de datos
+            return { message: "Asiento agregado con éxito" }; // Retorna un mensaje de éxito
 
         } catch (error) {
             if (this.connection) {
-                await this.connection.close(); 
+                await this.connection.close(); // Asegura que la conexión se cierre en caso de error
             }
-            console.log(error); 
+            console.error(error); // Muestra el error en la consola
         }
     }
 }
